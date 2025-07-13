3902be1f282eb7b1249f5029b328338c333799e0
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2025-07-12 21:55:33 -0300

    Fim do trabalho

--- frontend/src/pages/DashboardPage/components/OptionsMenu.js
+++ frontend/src/pages/DashboardPage/components/OptionsMenu.js
@@ -10,6 +10,7 @@ import ListItemIcon, { listItemIconClasses } from '@mui/material/ListItemIcon';
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
 import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded';
 import MenuButton from './MenuButton';
+import { useAuth } from '../../../contexts/AuthContext';
 
 const MenuItem = styled(MuiMenuItem)({
   margin: '2px 0',
@@ -24,6 +25,9 @@ export default function OptionsMenu() {
   const handleClose = () => {
     setAnchorEl(null);
   };
+  const {
+    logout
+  } = useAuth()
   return (
     <React.Fragment>
       <MenuButton
@@ -60,7 +64,7 @@ export default function OptionsMenu() {
         <MenuItem onClick={handleClose}>Settings</MenuItem>
         <Divider />
         <MenuItem
-          onClick={handleClose}
+          onClick={() => { handleClose(); logout() } }
           sx={{
             [`& .${listItemIconClasses.root}`]: {
               ml: 'auto',
