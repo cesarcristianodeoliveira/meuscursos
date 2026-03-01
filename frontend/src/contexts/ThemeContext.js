cec0d51a7a2015f50a27239a1d1ef07e83c2d083
Cesar Oliveira <cesarcristianodeoliveira@gmail.com>
2026-03-01 03:50:36 -0300

    New components and pages

--- frontend/src/contexts/ThemeContext.js
+++ frontend/src/contexts/ThemeContext.js
@@ -59,7 +59,7 @@ export const ThemeProviderWrapper = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ mode, toggleTheme, resolvedMode }}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme} disableTransitionOnChange>
         <CssBaseline />
         {children}
       </ThemeProvider>
